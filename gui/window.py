32dc888e8eb8e8ec1f7e22f2c607b98da5ede8f8
Wesley Silva <jwesleylima.developer@gmail.com>
2022-03-07 22:59:52 -0300

    Classe MainWindow adicionada

--- gui/window.py
+++ gui/window.py
@@ -6,6 +6,7 @@ Note: Some of these classes are of a lower level.
 
 
 import pygame
+from typing import Callable
 from gui.defutils import config_obj
 
 
@@ -28,7 +29,7 @@ class WindowParams:
 
 	@property
 	def window_width(self):
-		return self._window_height
+		return self._window_width
 
 	@window_width.setter
 	def window_width(self, new_width: int):
@@ -102,3 +103,82 @@ class WindowDisplayer(WindowDisplayerParams):
 		self._window.fill(self.background_color)
 		if self.main and self.main is not None:
 			self.main._draw(self._window)
+
+	def update(self) -> None:
+		pygame.display.flip()
+
+
+class WindowEventListener:
+	'''Responsible for listening to the user's interaction with the window.'''
+
+	def __init__(self):
+		self.listeners = {}
+
+	def bind(self, event_code: int, callback: Callable):
+		self.listeners[event_code] = callback
+
+	def listen(self):
+		for event in pygame.event.get():
+			if event.type in self.listeners.keys():
+				self.listeners[event.type](event)
+
+
+class MainWindow(WindowParams):
+	'''Main program window.'''
+	__started__ = False
+
+	def __init__(self, **kwargs: dict):
+		config_obj(target=self, **kwargs)
+		self._display = WindowDisplayer(
+			window_icon=self.window_icon,
+			window_title=self.window_title,
+			window_size=self.window_size
+		)
+		self.listener = WindowEventListener()
+
+		self.on_create()
+
+	####### Auxiliary Methods ######
+	def get_display_surface(self):
+		return self.display._window
+
+	def on_create(self):
+		'''Method called when the window is being created.
+
+		Ideal for declaring variables and instantiating classes'''
+		pass
+
+	def on_start(self):
+		'''Method called when the user interface is visible and usable.'''
+		pass
+
+	def on_exit(self, evt: object):
+		'''Method called when the user clicks to close the window.
+
+		Of course, by default it closes the window.'''
+		pygame.quit()
+		exit()
+
+	def update(self):
+		'''Method called on each user interface update.
+
+		Run your code that needs constant updating here.'''
+		pass
+
+	def bind_events(self):
+		'''Method for adding event listeners to the main window.'''
+		self.listener.bind(pygame.QUIT, self.on_exit)
+
+	def show(self):
+		'''Starts the main window loop.'''
+		self.bind_events()
+		while self.alive:
+			self._display.show()
+			# Calls the 'on_start' method if not already called
+			if not self.__started__:
+				self.__started__ = True
+				self.on_start()
+
+			self.listener.listen()
+			self.update()
+			self._display.update()
